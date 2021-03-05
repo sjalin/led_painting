fe93ef25e3f69a34f8ac463126b7b82dbbd2f50f
Sebastian Själin <sebastian.sjalin@xyleminc.com>
2021-03-05 15:19:21 +0100

    wip

--- contoller.py
+++ contoller.py
@@ -30,37 +30,40 @@ def gamepad(q: Queue):
 
     print('Gamepad found and started')
     # loop and filter by event code and print the mapped label
-    for event in gamepad_input.read_loop():
-        sleep(0.001)
-        tempdir = totdir
-        if event.type == ecodes.EV_KEY:
-            if event.value == 1:
-                if event.code == nvidia_btn:
-                    q.put('next game')
-                else:
-                    print(f'Unknown keypress: {event.code}')
-        elif event.type == ecodes.EV_ABS:
-            if event.code == dpad_x:
+    try:
+        for event in gamepad_input.read_loop():
+            sleep(0.001)
+            tempdir = totdir
+            if event.type == ecodes.EV_KEY:
                 if event.value == 1:
-                    xdir = 'e'
-                elif event.value == -1:
-                    xdir = 'w'
-                else:
-                    xdir = ''
-            if event.code == dpad_y:
-                if event.value == 1:
-                    ydir = 's'
-                elif event.value == -1:
-                    ydir = 'n'
-                else:
-                    ydir = ''
-            if xdir or ydir:
-                if bool(xdir) != bool(ydir):
-                    if xdir:
-                        tempdir = xdir
+                    if event.code == nvidia_btn:
+                        q.put('next game')
                     else:
-                        tempdir = ydir
+                        print(f'Unknown keypress: {event.code}')
+            elif event.type == ecodes.EV_ABS:
+                if event.code == dpad_x:
+                    if event.value == 1:
+                        xdir = 'e'
+                    elif event.value == -1:
+                        xdir = 'w'
+                    else:
+                        xdir = ''
+                if event.code == dpad_y:
+                    if event.value == 1:
+                        ydir = 's'
+                    elif event.value == -1:
+                        ydir = 'n'
+                    else:
+                        ydir = ''
+                if xdir or ydir:
+                    if bool(xdir) != bool(ydir):
+                        if xdir:
+                            tempdir = xdir
+                        else:
+                            tempdir = ydir
 
-            print(f'new dir {tempdir}')
-            totdir = tempdir
-            q.put('d:'+tempdir)
+                print(f'new dir {tempdir}')
+                totdir = tempdir
+                q.put('d:'+tempdir)
+    except * as e:
+        gamepad_connected_set(e)
